754c8ffc83c51a4819e62f60e03bbfb7fc590f92
Bartłomiej Bartoszewicz <bartlomiej.bartoszewicz95@gmail.com>
2026-08-13 21:38:42 +0200

    Initial Commit

---
+++ Refrigerator/RefrigeratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefrigeratorApp.swift
+//  Refrigerator
+//
+//  Created by Bartłomiej Bartoszewicz on 2026-08-13.
+//
+
+import SwiftUI
+
+@main
+struct RefrigeratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
